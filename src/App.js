dcc3b19c4443c4f7200ceb55777d0f2bda751881
mcg1nley <jjmc.ginley@btinternet.com>
2021-09-08 19:53:11 +0100

    08/09/21 from PC - Problems with app.js loading first Route (homepage)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,81 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
 import axios from "axios";
-
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
-  async componentDidMount() {
+  // Display default list of users
+  /*  async componentDidMount() {
     this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ users: res.data, loading: false });
-  }
+  } */
+
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
+    const { users, loading } = this.state;
+
     return (
-      <div>
-        <Navbar className="App" />
-        <div className="container">
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              {/* This following Route is not displaying */}
+              {/*     <Route
+                exact
+                path="/"
+                render={(props) => {
+                  <div>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </div>;
+                }}
+              /> */}
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import Search from "../users/Search";
+import Users from "../users/Users";
+
+export const Home = () => {
+  const { users, loading } = this.state;
+  return (
+    <Fragment>
+      <Search
+        searchUsers={this.searchUsers}
+        clearUsers={this.clearUsers}
+        showClear={users.length > 0 ? true : false}
+        setAlert={this.setAlert}
+      />
+      <Users loading={loading} users={users} />
+    </Fragment>
+  );
+};
+
+export default Home;
