08e5586867c32b917ec0a61c4a8456d198204485
mcg1nley <jjmc.ginley@btinternet.com>
2021-09-13 14:08:22 +0100

    app working as tutorial, fixed Alert Bug. (From Laptop 13/09/21)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,12 @@
-import React, { Fragment } from "react";
-import Search from "../users/Search";
-import Users from "../users/Users";
+import React, { Fragment } from 'react';
+import Search from '../users/Search';
+import Users from '../users/Users';
 
-const Home = () => {
-  const { users, loading } = this.state;
-  return (
-    <Fragment>
-      <Search
-        searchUsers={this.searchUsers}
-        clearUsers={this.clearUsers}
-        showClear={users.length > 0 ? true : false}
-        setAlert={this.setAlert}
-      />
-      <Users loading={loading} users={users} />
-    </Fragment>
-  );
-};
+const Home = () => (
+  <Fragment>
+    <Search />
+    <Users />
+  </Fragment>
+);
 
 export default Home;
